e91fe935eebf9667cee06951c0ae2f6a66139d78
Jordy Manner <jordy.manner@milkcreation.fr>
2018-09-17 05:39:49 +0000

    cookie-law -- ajustement mineur

--- templates/cookie-law.php
+++ templates/cookie-law.php
@@ -2,14 +2,15 @@
 ?>
 
 <?php
-tify_partial_cookie_notice(
-  [
-      'attrs'           => $attrs,
-      'content'         => $content . $policy,
-      'accept'          => $accept ,
-      'dismiss'         => $dismiss,
-      'cookie_name'     => $cookie_name,
-      'cookie_hash'     => $cookie_hash,
-      'cookie_expire'   => $cookie_expire,
-  ]
+echo partial(
+    'cookie_notice',
+    [
+        'attrs'         => $attrs,
+        'content'       => $content . $policy,
+        'accept'        => $accept,
+        'dismiss'       => $dismiss,
+        'cookie_name'   => $cookie_name,
+        'cookie_hash'   => $cookie_hash,
+        'cookie_expire' => $cookie_expire,
+    ]
 );
