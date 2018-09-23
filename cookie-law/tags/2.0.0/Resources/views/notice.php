3e4ea4a4932892725d26054981fa62a99d1016d1
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2018-09-23 12:54:33 +0000

    cookie-law -- tag 2.0.0 -- réécriture

---
+++ cookie-law/tags/2.0.0/Resources/views/notice.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @var tiFy\Contracts\Views\ViewInterface $this
+ */
+?>
+
+<?php $this->insert('text', $this->all()); ?>
+
+<?php $this->insert('accept', $this->all()); ?>
+
+<?php $this->insert('privacy-policy', $this->all()); ?>
+
+<?php $this->insert('close', $this->all()); ?>
