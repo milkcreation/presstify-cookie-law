0cadfb888b659aa67fc434e0bbe8fd7b83c48b10
Jordy Manner <jordy.manner@milkcreation.fr>
2018-10-05 10:43:05 +0000

    cookie-law -- tag 2.0.2 -- correctifs

---
+++ Resources/assets/index.js
@@ -0,0 +1,11 @@
+/** Sass */
+import './scss/styles.scss';
+
+/** jQuery */
+import 'jquery';
+
+/** Presstify Framework */
+/** cookie-notice */
+import 'presstify-framework/partial/cookie-notice/index';
+/** modal */
+import 'presstify-framework/partial/modal/index';
