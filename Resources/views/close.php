c97940c54e96eebb1de723bf62289c1ba0f989d1
Jordy Manner <jordy.manner@milkcreation.fr>
2019-01-07 17:25:38 +0000

    tag 2.0.5 -- Evolution tiFy 2.0 > PageHook

---
+++ Resources/views/close.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @var tiFy\Contracts\View\ViewController $this
+ */
+?>
+
+<?php
+echo partial('tag', [
+    'tag'     => 'a',
+    'attrs'   => [
+        'class'       => 'CookieLaw-close',
+        'href'        => "#{$this->get('id')}",
+        'aria-toggle' => 'dismiss'
+    ],
+    'content' => __('Fermer', 'theme')
+]);
