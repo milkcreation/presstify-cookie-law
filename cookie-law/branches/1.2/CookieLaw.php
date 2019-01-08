22ad647100d61893d66bf29f9f64b7bedf552818
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-01-08 12:06:37 +0000

    cookie-law -- branch 1.2 -- déploiement initial

--- cookie-law/branches/1.2/CookieLaw.php
+++ cookie-law/branches/1.2/CookieLaw.php
@@ -1,183 +1,76 @@
 <?php
+/*
+Plugin Name: Cookie Law
+Plugin URI: http://presstify.com/plugins/cookie-law
+Description: Politique des cookies
+Version: 1.2.0
+Author: Milkcreation
+Author URI: http://milkcreation.fr
+*/
 
-/**
- * @name CookieLaw
- * @desc Extension PresstiFy de notification des règles cookie du site.
- * @author Jordy Manner <jordy@tigreblanc.fr>
- * @package presstiFy
- * @namespace tiFy\Plugins\CookieLaw
- * @version 0.0.0
- */
+namespace tiFy\Plugins\CookieLaw; 
 
-namespace tiFy\Plugins\CookieLaw;
-
-use tiFy\App\Plugin;
-use tiFy\Core\Router\Router;
-
-class CookieLaw extends Plugin
+class CookieLaw extends \tiFy\App\Plugin
 {
-    /**
-     * Classe de rappel du Router
-     * @var \tiFy\Core\Router\Factory
-     */
-    private static $Router = null;
-
-    /**
-     * CONSTRUCTEUR
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-
-        // Définition des événements de déclenchement
-        $this->tFyAppAddAction('init');
-        $this->tFyAppAddAction('tify_router_register');
-        $this->tFyAppAddAction('wp_enqueue_scripts');
-        $this->tFyAppAddAction('wp_footer');
-    }
-
-    /**
-     * DECLENCHEURS
-     */
-    /**
-     * Initialisation globale
-     *
-     * @return void
-     */
-    public function init()
-    {
-        // Déclaration des scripts
-        \wp_register_style(
-            'tiFySet_CookieLaw',
-            self::tFyAppAssetsUrl('CookieLaw.css', get_class()),
-            ['dashicons'],
-            141118
-        );
-    }
-
-    /**
-     * Déclaration des attributs de routage de la page d'affichage des règles de cookie
-     *
-     * @return void
-     */
-    public function tify_router_register()
-    {
-        if (!$attrs = self::tFyAppConfig('router')) :
-            return;
-        endif;
-
-        $defaults = [
-            'title' => __('Page d\'affichage des règles de cookie', 'tify')
-        ];
-        if (!is_array($attrs)) :
-            $attrs = [];
-        endif;
-
-        $attrs = \wp_parse_args($attrs, $defaults);
-
-        self::$Router = Router::register('_tiFySet_CookieLaw', $attrs);
-    }
-
-    /**
-     * Initialisation des scripts de l'interface utilisateur
-     *
-     * @return void
-     */
-    public function wp_enqueue_scripts()
-    {
-        if(self::tFyAppConfig('enqueue_scripts', true)) :
-            self::enqueue_scripts();
-        endif;
-    }
-
-    /**
-     * Pied de page de l'interface utilisateur du site
-     *
-     * @return string
-     */
-    public function wp_footer()
-    {
-        if(self::tFyAppConfig('display', true)) :
-            self::display();
-        endif;
-    }
-
-    /**
-     * CONTROLEURS
-     */
-    /**
-     * Affichage
-     *
-     * @param bool $echo Activation de l'affichage
-     *
-     * @return string
-     */
-    final public function display($echo = true)
-    {
-        // Définition des arguments du gabarit
-        $container_id = 'tiFySet-CookieLaw';
-        $text = self::tFyAppConfig('text');
-        if($rules_page_id = self::getRulesPageId()) :
-            $rules_url = \get_permalink($rules_page_id);
-        else :
-            $rules_url = '';
-        endif;
-        $valid_text = self::tFyAppConfig('valid_text');
-        $rules_text = self::tFyAppConfig('rules_text');
-        $close_text = self::tFyAppConfig('close_text');
-
-        // Récupération du gabarit
-        ob_start();
-        self::tFyAppGetTemplatePart(
-            'notice',
-            null,
-            compact(
-                'container_id',
-                'text',
-                'rules_url',
-                'valid_text',
-                'rules_text',
-                'close_text'
-            )
-        );
-        $text = ob_get_clean();
-
-        // Affichage de la notification
-        Control::CookieNotice(
-            [
-                'id'              => 'tiFySet-CookieLaw',
-                'container_id'    => $container_id,
-                'cookie_name'     => 'tiFySet_CookieLaw',
-                'cookie_hash'     => true,
-                'cookie_expire'   => YEAR_IN_SECONDS,
-                'text'            => $text,
-            ],
-            $echo
-        );
-    }
-
-    /**
-     * Mise en file des scripts
-     */
-    final public static function enqueue_scripts()
-    {
-        \wp_enqueue_style('tiFySet_CookieLaw');
-        Control::enqueue_scripts('cookie_notice');
-    }
-
-    /**
-     * Récupération de l'identifiant de qualification de la page d'affichage des règles de cookie
-     *
-     * @return int
-     */
-    final public static function getRulesPageId()
-    {
-        if (!$router = self::$Router) :
-            return 0;
-        endif;
-
-        return $router->getSelected();
-    }
+	/* = ARGUMENTS = */
+	/** == ACTIONS == **/
+	// Liste des Actions à déclencher
+	protected $tFyAppActions				= array( 
+		'init',
+		'admin_init',
+		'wp_enqueue_scripts',
+		'wp_footer',
+		'wp_ajax_tiFy_CookieLaw',
+		'wp_ajax_nopriv_tiFy_CookieLaw'
+	);	
+	// Fonctions de rappel des actions
+	protected $tFyAppActionsMethods	= array(
+		'wp_ajax_tiFy_CookieLaw' 			=> 'wp_ajax',
+		'wp_ajax_nopriv_tiFy_CookieLaw'		=> 'wp_ajax'
+	);
+			
+	/* = ACTIONS = */
+	/** == Initialisation globale == **/
+	public function init()
+	{
+		if( $post_id = get_option( 'page_for_cookie_law', 0 ) )
+			self::tFyAppConfigSetAttr('post_id', $post_id);
+		
+		// Déclaration des scripts
+		wp_register_style( 'tiFy_CookieLaw', self::tFyAppUrl() . '/CookieLaw.css', array( 'dashicons' ), '141118' );
+		wp_register_script( 'tiFy_CookieLaw', self::tFyAppUrl() . '/CookieLaw.js', array( 'jquery' ), '141118', true );
+	}
+	
+	/** == Instanciation des scripts == **/
+	public function wp_enqueue_scripts()
+	{
+		wp_enqueue_script( 'tiFy_CookieLaw' );
+		wp_enqueue_style( 'tiFy_CookieLaw' );
+	}
+	
+	/** == Affichage == **/
+	public function wp_footer()
+	{
+		$output  = "";
+		$output .= "\t<div id=\"tiFy_CookieLaw\" style=\"". ( ( isset( $_COOKIE[ 'tify_cookie_law_'. COOKIEHASH ] ) && ( $_COOKIE[ 'tify_cookie_law_'.COOKIEHASH ] == true ) ) ? 'display:none;': '' ) ."\">\n";
+		$output .= "\t\t<p>" . self::tFyAppConfig( 'text' ) ."</p>\n";
+		$output .= "\t\t<a href=\"#tiFy_CookieLaw-accept\" id=\"tiFy_CookieLaw-accept\">". __( 'Accepter', 'tify' ) ."</a>&nbsp;&nbsp;";
+		if( self::tFyAppConfig( 'post_id' ) && ( $read_url = get_permalink( self::tFyAppConfig( 'post_id' ) ) ) )
+			$output .= "\t\t<a href=\"{$read_url}\" id=\"tiFy_CookieLaw-read\" target=\"_blank\">". __( 'En savoir plus', 'tify' ) ."</a>\n";
+		$output .= "\t\t<a href=\"#tiFy_CookieLaw-close\" id=\"tiFy_CookieLaw-close\" >" . self::tFyAppConfig( 'close_text' ) ."</a>\n";
+		$output .= "\t</div>\n";
+		
+		echo $output;
+	}
+	
+	/** == Définition du cookie == **/
+	public function wp_ajax()
+	{
+		$expire = (int) self::tFyAppConfig( 'cookie_expire' );
+		$secure = ( 'https' === parse_url( home_url(), PHP_URL_SCHEME ) );
+		setcookie( 'tify_cookie_law_'. COOKIEHASH, true, time() + $expire, COOKIEPATH, COOKIE_DOMAIN, $secure, true );
+		if ( COOKIEPATH != SITECOOKIEPATH )
+			setcookie( 'tify_cookie_law_'. COOKIEHASH, true, time() + $expire, SITECOOKIEPATH, COOKIE_DOMAIN, $secure, true );
+		wp_die(1);
+	}
 }
