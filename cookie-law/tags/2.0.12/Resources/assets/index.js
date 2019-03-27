70f9b418a58b8368e8396b78a9aa20ca449b9286
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-03-27 15:38:44 +0000

    cookie-law -- branch 2.0 -- Maj tiFy Injection de dépendances

---
+++ cookie-law/tags/2.0.12/Resources/assets/index.js
@@ -0,0 +1,12 @@
+/** Css */
+import './css/styles.css';
+
+/** jQuery */
+import 'jquery';
+
+/** Presstify Framework */
+/** cookie-notice */
+import 'presstify-framework/partial/cookie-notice/index';
+
+/** modal */
+import 'presstify-framework/partial/modal/index';
