91b740aa68ee689e9507ceeaff2ea61434952198
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-06-12 15:40:09 +0000

    cookie-law -- tag 2.0.15 -- maj assets tiFy

---
+++ cookie-law/tags/2.0.15/Resources/views/notice-text.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Cookie Law - Notification | Texte.
+ * ---------------------------------------------------------------------------------------------------------------------
+ *
+ * @var tiFy\Plugins\CookieLaw\CookieLawView $this
+ */
+?>
+<div class="CookieLaw-text">
+    <?php
+    _e(
+        'En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies pour vous proposer ' .
+        'des services et offres adaptés à vos centres d’intérêts.',
+        'tify'
+    );
+    ?>
+</div>
