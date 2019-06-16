c6bbde43c625a4088c52cb97394ae9b74be79b5b
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-06-16 08:58:40 +0000

    cookie-law -- tag 2.0.16 -- assets css + scss > FilesWatcher

---
+++ cookie-law/tags/2.0.16/Resources/views/modal-header.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Cookie Law - Modale | Entête.
+ * ---------------------------------------------------------------------------------------------------------------------
+ *
+ * @var tiFy\Plugins\CookieLaw\CookieLawView $this
+ */
+?>
+<div class="modal-header">
+    <h5 class="modal-title"><?php echo $this->privacyPolicy()->getTitle(); ?></h5>
+
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
