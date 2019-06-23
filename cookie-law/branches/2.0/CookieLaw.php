f6aeaaa87e940ccefb58b8732b5363208147efd7
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-06-23 18:17:00 +0000

    cookie-law -- branch 2.0 -- branchement tiFyCookie

--- cookie-law/branches/2.0/CookieLaw.php
+++ cookie-law/branches/2.0/CookieLaw.php
@@ -13,7 +13,7 @@ use tiFy\Support\{ParamsBag, Proxy\Partial};
  * @desc Extension PresstiFy d'affichage des règles de cookie.
  * @author Jordy Manner <jordy@tigreblanc.fr>
  * @package tiFy\Plugins\CookieLaw
- * @version 2.0.17
+ * @version 2.0.18
  *
  * USAGE :
  * Activation
--- cookie-law/branches/2.0/Resources/views/notice-text.php
+++ cookie-law/branches/2.0/Resources/views/notice-text.php
@@ -7,11 +7,9 @@
  */
 ?>
 <div class="CookieLaw-text">
-    <?php
-    _e(
+    <?php _e(
         'En poursuivant votre navigation sur ce site, vous acceptez l’utilisation de cookies pour vous proposer ' .
         'des services et offres adaptés à vos centres d’intérêts.',
         'tify'
-    );
-    ?>
+    ); ?>
 </div>
