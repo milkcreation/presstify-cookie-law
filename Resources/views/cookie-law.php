292a05e5f710a95daa9950f60bc0ad9419f1cd5a
Jordy Manner <jordy.manner@milkcreation.fr>
2019-06-23 18:17:00 +0000

    cookie-law -- branch 2.0 -- branchement tiFyCookie

--- Resources/views/cookie-law.php
+++ Resources/views/cookie-law.php
@@ -7,15 +7,16 @@
 ?>
 <?php $this->insert('privacy-policy', $this->all()); ?>
 
-<?php
-echo partial('cookie-notice', [
-    'attrs'         => [
+<?php echo partial('cookie-notice', [
+    'attrs'   => [
         'id' => $this->get('id'),
     ],
-    'content'       => $this->fetch('notice', $this->all()),
-    'cookie_name'   => 'CookieLaw',
-    'cookie_expire' => YEAR_IN_SECONDS,
-    'dismiss'       => false,
-    'type'          => 'info',
-    'trigger'       => false
+    'content' => $this->fetch('notice', $this->all()),
+    'cookie'  => [
+        'name'   => 'CookieLaw',
+        'expire' => YEAR_IN_SECONDS,
+    ],
+    'dismiss' => false,
+    'type'    => 'info',
+    'trigger' => false,
 ]);
