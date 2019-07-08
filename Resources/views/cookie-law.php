1723976fb1bf53143580288128004f3eecd1d183
Jordy Manner <jordy.manner@milkcreation.fr>
2019-07-08 09:44:05 +0000

    cookie-law -- tag 2.0.19 -- Ajustement animation fadeOut + Corrections z-index backdrop

---
+++ Resources/views/cookie-law.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Cookie Law - Gabarit principal
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Plugins\CookieLaw\CookieLawView $this
+ */
+?>
+<?php $this->insert('privacy-policy', $this->all()); ?>
+
+<?php echo partial('cookie-notice', [
+    'attrs'   => [
+        'id' => $this->get('id'),
+    ],
+    'content' => $this->fetch('notice', $this->all()),
+    'cookie'  => [
+        'name'   => 'CookieLaw',
+        'expire' => YEAR_IN_SECONDS,
+    ],
+    'dismiss' => false,
+    'type'    => 'info',
+    'trigger' => false,
+]);
