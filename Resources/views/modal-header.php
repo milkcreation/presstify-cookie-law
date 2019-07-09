0720a38351ee68a72d73f88c796b3c23a4e5c503
Jordy Manner <jordy.manner@milkcreation.fr>
2019-07-09 17:09:16 +0000

    cookie-law -- tag 2.0.20 -- correctif notice fixed

---
+++ Resources/views/modal-header.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Cookie Law - Modale | Entête.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Plugins\CookieLaw\CookieLawView $this
+ */
+?>
+<div class="modal-header">
+    <h5 class="modal-title"><?php echo $this->get('privacy_policy.title'); ?></h5>
+
+    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
