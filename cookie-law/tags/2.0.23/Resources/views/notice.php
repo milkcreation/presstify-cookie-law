227019266e5c20826242f463125371d7665ce445
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-09-23 15:34:12 +0000

    cookie-law -- tag 2.0.23 -- Correctif assets

---
+++ cookie-law/tags/2.0.23/Resources/views/notice.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Cookie Law - Notification.
+ * ---------------------------------------------------------------------------------------------------------------------
+ *
+ * @var tiFy\Plugins\CookieLaw\CookieLawView $this
+ */
+?>
+<div class="CookieLaw-notice">
+    <?php $this->insert('notice-text', $this->all()); ?>
+
+    <?php $this->insert('notice-accept', $this->all()); ?>
+
+    <?php $this->insert('notice-read', $this->all()); ?>
+
+    <?php $this->insert('notice-close', $this->all()); ?>
+</div>
+
+<div class="CookieLaw-backdrop"></div>
