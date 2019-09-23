3930d6826abd27e905022d20d9a15f99d5b6ecab
Jordy Manner <jordy.manner@milkcreation.fr>
2019-09-23 15:32:43 +0000

    cookie-law -- branch 2.0 -- Correctif assets

--- Resources/assets/index.js
+++ Resources/assets/index.js
@@ -1,12 +1,2 @@
-// DEPS
-// jQuery
-import 'jquery';
-
-// Presstify - Partial > cookie-notice
-import 'presstify-framework/partial/cookie-notice/index';
-
-// Presstify - Partial > modal
-import 'presstify-framework/partial/modal/index';
-
-// CSS
 import './scss/styles.scss';
+import './js/scripts';
