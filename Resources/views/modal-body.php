bbce5d3036e84f7b8f75362ba4472ec0e5fd5105
Jordy Manner <jordy.manner@milkcreation.fr>
2019-09-23 15:34:12 +0000

    cookie-law -- tag 2.0.23 -- Correctif assets

---
+++ Resources/views/modal-body.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Cookie Law - Modale | Corps.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Plugins\CookieLaw\CookieLawView $this
+ */
+?>
+<div class="modal-body">
+    <?php echo $this->get('privacy_policy.content'); ?>
+</div>
