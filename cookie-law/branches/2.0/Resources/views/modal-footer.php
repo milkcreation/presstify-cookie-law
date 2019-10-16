ff5d63587d51bf69062a605c13c4db7666f32db6
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-10-16 15:30:04 +0000

    cookie-law -- branch 2.0 -- Adaptation réécriture modal tiFy

--- cookie-law/branches/2.0/Resources/views/modal-footer.php
+++ cookie-law/branches/2.0/Resources/views/modal-footer.php
@@ -5,8 +5,6 @@
  * @var tiFy\Plugins\CookieLaw\CookieLawView $this
  */
 ?>
-<div class="modal-footer">
-    <button type="button" class="btn btn-secondary button-secondary" data-dismiss="modal">
-        <?php _e('Fermer', 'tify'); ?>
-    </button>
-</div>
+<button type="button" class="btn btn-secondary button-secondary" data-control="modal.close">
+    <?php _e('Fermer', 'tify'); ?>
+</button>
