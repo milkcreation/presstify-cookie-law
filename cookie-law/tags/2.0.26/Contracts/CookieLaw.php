806e973bcb634b56fd5aa6d9772d42c61f90c15e
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-10-21 16:48:31 +0000

    cookie-law -- tag 2.0.26 -- Chanine caractère theme >> tify

---
+++ cookie-law/tags/2.0.26/Contracts/CookieLaw.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\CookieLaw\Contracts;
+
+use tiFy\Contracts\Partial\Modal;
+use tiFy\Contracts\View\ViewController;
+use tiFy\Contracts\View\ViewEngine;
+use tiFy\Contracts\Support\ParamsBag;
+
+interface CookieLaw extends ParamsBag
+{
+    /**
+     * Résolution de sortie de la classe en tant que chaîne de caractère.
+     *
+     * @return string
+     */
+    public function __toString(): string;
+
+    /**
+     * @inheritDoc
+     *
+     * @return array
+     */
+    public function defaults(): array;
+
+    /**
+     * Affichage du bandeau de validation.
+     *
+     * @return string
+     */
+    public function display(): string;
+
+    /**
+     * Récupération de la modale d'affichage de la politique de confidentialité.
+     *
+     * @return Modal|null
+     */
+    public function modal(): ?Modal;
+
+    /**
+     * @inheritDoc
+     *
+     * @return static
+     */
+    public function parse(): CookieLaw;
+
+    /**
+     * Récupération d'un instance du controleur de liste des gabarits d'affichage ou d'un gabarit d'affichage.
+     * {@internal Si aucun argument n'est passé à la méthode, retourne l'instance du controleur de liste.}
+     * {@internal Sinon récupére l'instance du gabarit d'affichage et passe les variables en argument.}
+     *
+     * @param string|null view Nom de qualification du gabarit.
+     * @param array $data Liste des variables passées en argument.
+     *
+     * @return ViewController|ViewEngine
+     */
+    public function viewer(?string $view = null, array $data = []);
+}
---
+++ cookie-law/tags/2.0.26/Resources/views/modal-footer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Cookie Law - Modale | Pied.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Plugins\CookieLaw\CookieLawView $this
+ */
+?>
+<button type="button" class="btn btn-secondary button-secondary" data-control="modal.close">
+    <?php _e('Fermer', 'tify'); ?>
+</button>
