b316fffd32754305d659823b12148828acd03ea2
mastermilk <mastermilk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2019-12-11 11:35:57 +0000

    cookie-law -- branch 1.4 -- Dépôt pour bridge

---
+++ cookie-law/branches/1.4/Resources/views/modal-body.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Cookie Law - Modale | Corps.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Plugins\CookieLaw\CookieLawView $this
+ */
+echo $this->get('privacy_policy.content');
