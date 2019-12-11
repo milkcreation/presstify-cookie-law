d4659e8465271f72b4c3ea0037450191ae8ffde7
Jordy Manner <jordy.manner@milkcreation.fr>
2019-12-11 11:35:57 +0000

    cookie-law -- branch 1.4 -- Dépôt pour bridge

---
+++ Resources/views/modal-header.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Cookie Law - Modale | Entête.
+ * ---------------------------------------------------------------------------------------------------------------------
+ * @var tiFy\Plugins\CookieLaw\CookieLawView $this
+ */
+?>
+<h5 class="modal-title"><?php echo $this->get('privacy_policy.title'); ?></h5>
