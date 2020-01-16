d30fc9d63ca591d7c6951b1fe880e68f848eb108
mast3rm1lk <mast3rm1lk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-01-16 06:06:50 +0000

    cookie-law -- branch 2.0 -- Passage dépôt en https

--- cookie-law/branches/2.0/CookieLaw.php
+++ cookie-law/branches/2.0/CookieLaw.php
@@ -11,7 +11,7 @@ use tiFy\Support\{ParamsBag, Proxy\Partial};
  * @desc Extension PresstiFy d'affichage des règles de cookie.
  * @author Jordy Manner <jordy@tigreblanc.fr>
  * @package tiFy\Plugins\CookieLaw
- * @version 2.0.30
+ * @version 2.0.31
  *
  * USAGE :
  * Activation
