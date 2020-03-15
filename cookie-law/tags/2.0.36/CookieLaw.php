66c0bf862a50004a4e207c87ba3999c24b53ae12
mast3rm1lk <mast3rm1lk@9c4951e9-fce0-42f0-91e9-f5562070ff10>
2020-03-15 20:31:08 +0000

    cookie-law -- 2.0.36 -- Evolution réécriture vues

---
+++ cookie-law/tags/2.0.36/CookieLaw.php
@@ -0,0 +1,152 @@
+<?php declare(strict_types=1);
+
+namespace tiFy\Plugins\CookieLaw;
+
+use Psr\Container\ContainerInterface as Container;
+use tiFy\Contracts\Partial\Modal;
+use tiFy\Plugins\CookieLaw\Contracts\CookieLaw as CookieLawContract;
+use tiFy\Support\{ParamsBag, Proxy\Partial};
+
+/**
+ * @desc Extension PresstiFy d'affichage des règles de cookie.
+ * @author Jordy Manner <jordy@tigreblanc.fr>
+ * @package tiFy\Plugins\CookieLaw
+ * @version 2.0.36
+ *
+ * USAGE :
+ * Activation
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Dans config/app.php ajouter \tiFy\Plugins\CookieLaw\CookieLawServiceProvider à la liste des fournisseurs de services.
+ * ex.
+ * <?php
+ * ...
+ * use tiFy\Plugins\CookieLaw\CookieLawServiceProvider;
+ * ...
+ *
+ * return [
+ *      ...
+ *      'providers' => [
+ *          ...
+ *          CookieLawServiceProvider::class
+ *          ...
+ *      ]
+ * ];
+ *
+ * Configuration
+ * ---------------------------------------------------------------------------------------------------------------------
+ * Dans le dossier de config, créer le fichier cookie-law.php
+ * @see /vendor/presstify-plugins/cookie-law/Resources/config/cookie-law.php
+ */
+class CookieLaw extends ParamsBag implements CookieLawContract
+{
+    /**
+     * Instance du conteneur d'injection de dépendances.
+     * @var Container
+     */
+    protected $container;
+
+    /**
+     * Instance de la fenêtre modal d'affichage de la politique de confidentialité.
+     * @var null|false|Modal
+     */
+    protected $modal;
+
+    /**
+     * CONSTRUCTEUR.
+     *
+     * @param Container|null $container Instance du conteneur d'injection de dépendances.
+     *
+     * @return void
+     */
+    public function __construct(?Container $container = null)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function __toString(): string
+    {
+        return (string)$this->render();
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return array
+     */
+    public function defaults(): array
+    {
+        return [
+            'modal'          => true,
+            'privacy_policy' => [],
+            'viewer'         => [],
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function modal(): ?Modal
+    {
+        if (is_null($this->modal) && $this->get('modal')) {
+            foreach (['header', 'body', 'footer'] as $part) {
+                if (!$this->has("modal.content.{$part}")) {
+                    $this->set("modal.content.{$part}", $this->viewer("modal-{$part}", $this->all()));
+                }
+            }
+
+            $this->modal = Partial::get('modal', 'cookieLaw-privacyPolicy', array_merge([
+                'attrs'     => [
+                    'id' => 'Modal-cookieLaw-privacyPolicy',
+                ],
+                'options'   => ['show' => false, 'backdrop' => true],
+                'size'      => 'xl',
+                'in_footer' => false,
+            ], $this->get('modal', [])));
+        }
+
+        return $this->modal;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parse(): CookieLawContract
+    {
+        parent::parse();
+
+        $this->set([
+            'id'             => 'CookieLaw',
+            'privacy_policy' => [
+                'content' => $this->viewer('default-txt'),
+                'title'   => $this->viewer('default-title'),
+            ],
+        ]);
+
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function render(): string
+    {
+        return (string)$this->viewer('index', $this->all());
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function viewer(?string $view = null, array $data = [])
+    {
+        $viewer = $this->container->get('cookie-law.view');
+
+        if (func_num_args() === 0) {
+            return $viewer;
+        }
+
+        return $viewer->render($view, $data);
+    }
+}
